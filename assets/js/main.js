2b7bd2c6774cd4b616574271345c4e01a9b14adc
Ines Montani <ines.montani@gmail.com>
2016-05-26 15:33:03 +0200

    Update to new design

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-// Navigation
-var nav = document.getElementById('nav');
-
-document.getElementById('nav-icon').addEventListener('click', function(e) {
-  e.preventDefault();
-  (nav.classList.contains('active')) ? nav.classList.remove('active') : nav.classList.add('active');
-});
